8c689521102ffe09b88e3751810f2baa45e5bcb9
Mikemupararano <kudath@yahoo.co.uk>
2024-07-11 22:37:31 +0100

    Written code to handle changes to first name, last name, email, phone, subjects, url and about.

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,62 @@
 import react from 'react';
 import './App.css'
 function App() {
+  const [values, setValues] = react.useState({
+    firstname: '',
+    lastname: '',
+    email: '',
+    phone: '',
+    gender: '',
+    subjects: '',
+    resume: '',
+    url: '',
+    about: ''
+    
+})
 
-
+  const handleChanges = (e) => {
+    setValues({...values, [e.target.name, e.target.values]})
+  }
   return (
     <>
       <div className="container">
         <h1>Dynamic React Form</h1>
-        <form>
+        <form >
           <label htmlFor='name'>First Name</label>
-          <input type="text" placeholder='Enter your first name' name='firstname' required />
+          <input type="text" placeholder='Enter your first name' name='firstname' required
+            onChange={(e) => handleChanges(e)} />
           <label htmlFor='lastname'>Last Name</label>
-          <input type="text" placeholder='Enter your last name' name='lastname' required />
+          <input type="text" placeholder='Enter your last name' name='lastname' required
+          onChange={(e) => handleChanges(e)}/>
           <label htmlFor='email'>Email</label>
-          <input type="email" placeholder='Enter your email' name='email' required />
+          <input type="email" placeholder='Enter your email' name='email' required
+          onChange={(e) => handleChanges(e)}/>
           <label htmlFor='phone'>Phone</label>
-          <input type="text" placeholder='Enter your phone' name='phone' required />
+          <input type="text" placeholder='Enter your phone' name='phone' required
+          onChange={(e) => handleChanges(e)}/>
           <label htmlFor='gender'>Gender</label>
-          <input type="radio" name='gender' /> Male
-          <input type="radio" name='gender' /> Female
-          <input type="radio" name='gender' /> Other
+          <input type="radio" name='gender'
+          onChange={(e) => handleChanges(e)}/> Male
+          <input type="radio" name='gender'
+          onChange={(e) => handleChanges(e)}/> Female
+          <input type="radio" name='gender'
+            onChange={(e) => handleChanges(e)}/> Other
           <label htmlFor='subjects'>Subjects</label>
-          <select name='subjects' id='subjects'>
+          <select name='subjects' id='subjects' onChange={(e) => handleChanges(e)}>
             <option value="english">English</option>
             <option value="maths">Maths</option>
             <option value="physics">Physics</option>
             <option value="chemistry">Chemistry</option>
             <label htmlFor='resume'>Resume</label>
           </select>
-          <input type="file" placeholder='Upload your resume' name='resume' />
+          <input type="file" placeholder='Upload your resume' name='resume'
+          onChange={(e) => handleChanges(e)}/>
           <label htmlFor='url'>URL</label>
-          <input type="text" name='url' placeholder='Enter your url' />
+          <input type="text" name='url' placeholder='Enter your url'
+          onChange={(e) => handleChanges(e)}/>
           <label htmlFor='about'>About</label>
-          <textarea name='about' id='about' cols="30" rows="10" placeholder='Write a description'></textarea>
+          <textarea name='about' id='about' cols="30" rows="10" 
+          onChange={(e) => handleChanges(e)}placeholder='Write a description'></textarea>
           <button type='reset'>Reset</button>
           <button type='submit'>Submit</button>
 
